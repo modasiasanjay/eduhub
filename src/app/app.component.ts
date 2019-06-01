d3866a50e80f577c7c7cf9f06e3cca18ec71f467
modasiasanjay <modasiasanjay94@gmail.com>
2019-06-01 06:07:22 +0530

    Project setup Srcdir

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'eduhub-project';
+  constructor(private router:Router){}
+  isValid(): boolean {
+    if ((this.router.url == '/admin')) {
+              return false;
+      }
+    
+    else if ((this.router.url == '/admin/addcourses')) {
+      return false;
+}
+else if ((this.router.url == '/admin/addcourses/videos')) {
+  return false;
+} 
+
+
+return true;
+  }
+}
